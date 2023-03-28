b04331fa88d900ff754b23f2408733113795829c
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-03-27 21:02:45 -0700

    try declspec

--- shared.c
+++ shared.c
@@ -6,11 +6,17 @@ SQLITE_EXTENSION_INIT3
 // hook to call into golang functionality defined in go.riyazali.net/sqlite
 extern int go_sqlite3_extension_init(const char*, sqlite3*, char**);
 
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
 int sqlite3_http_init(sqlite3* db, char** pzErrMsg, const sqlite3_api_routines *pApi) {
 	SQLITE_EXTENSION_INIT2(pApi)
 	return go_sqlite3_extension_init("http", db, pzErrMsg);
 }
 
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
 int sqlite3_http_no_network_init(sqlite3* db, char** pzErrMsg, const sqlite3_api_routines *pApi) {
 	SQLITE_EXTENSION_INIT2(pApi)
 	return go_sqlite3_extension_init("http_no_network", db, pzErrMsg);
